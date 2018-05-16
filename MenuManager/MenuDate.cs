dfddf138ff57de4c39077ede658f924185499a77
Madhura Pattekar <madhuracp@hotmail.com>
2018-05-16 10:48:02 -0700

    5/16 commit

---
+++ MenuManager/MenuDate.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MenuManager
+{
+   public class MenuDate
+    {
+        [Key]
+        public int Id { get; set; }
+        [ForeignKey("MenuItem")]
+        public int MenuID { get; set; }
+        public virtual MenuItem Item { get; set; }
+        public DateTime DateOfMenu { get; set; }
+
+    }
+}
---
+++ MenuManager/MenuItem.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MenuManager
+{
+    /// <summary>
+    /// Enum declared for categories of food item
+    /// </summary>
+    public enum CategoryOfFood
+    {
+        Daal,
+        Enter,
+        Rice,
+        Paratha,
+        Dessert,
+        Raita
+    };
+    public class MenuItem
+    {
+        /// <summary>
+        /// Id of MenuItem
+        /// </summary>
+        [Key]
+        public int Id { get; set; }
+        /// <summary>
+        /// Property for name of the food item
+        /// </summary>
+        public string Name { get; set; }
+        public CategoryOfFood Category { get; set; }
+        public virtual ICollection<MenuDate> Dates { get; set; }
+        public MenuItem()
+        {
+            Dates = new List<MenuDate>();
+        }
+       
+    }
+}
